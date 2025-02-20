0aeadeaafc9f40ab9291d3d3bd8433d03560236d
Akshayakumar K M <akshayakumar.k.m@ibm.com>
2025-02-20 21:15:16 +0530

    solution for coding of look & Say Sequence

---
+++ src/com/self/practise/solutionForLookAndSaySequence.java
@@ -0,0 +1,37 @@
+package com.self.practise;
+
+
+public class solutionForLookAndSaySequence {
+
+    public static String lookAndSaySequence(int n) {
+        if(n==1){
+            return "1";
+        }
+        else if( n==2){
+            return "11";
+        }
+        String res = "11";
+        for(int i =3;i<=n;i++){
+            res +='$';
+            int len = res.length();
+            int count =1;
+            StringBuilder temp = new StringBuilder();
+            for(int j =1;j<len;j++){
+                if (res.charAt(j) != res.charAt(j-1)){
+                    temp.append(count).append(res.charAt(j-1));
+                    count =1;
+                } else{
+                    count++;
+                }
+            }
+            res = temp.toString();
+        }
+
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(lookAndSaySequence(6));
+    }
+ }
