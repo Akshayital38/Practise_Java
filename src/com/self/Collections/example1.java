69d7d0f8ace537a8a2eb1a8cc2018011a7f463c5
Akshayakumar K M <akshayakumar.k.m@ibm.com>
2025-03-24 19:38:18 +0530

    Example for 1 and 2

--- src/com/self/Collections/example1.java
+++ src/com/self/Collections/example1.java
@@ -6,17 +6,33 @@ import java.util.List;
 import java.util.Set;
 
 public class example1 {
-    public static void main(String[] args) {
-        List<Integer> val = new ArrayList<>();
-        Set<Integer> val2 = new HashSet<>();
-        val.add(12);
-        val.add(23);
-        val.add(1);
-        val.add(23);
-        val.add(12);
 
-        HashSet<Integer> res = new HashSet<>(val);
+    public static List<Integer> removeElement(int[] nums, int val) {
+        List<Integer> res = new ArrayList<>();
+        for(int i =0; i<nums.length; i++){
+            if(  nums[i] != val){
+                res.add(nums[i]);
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+//        List<Integer> val = new ArrayList<>();
+//        Set<Integer> val2 = new HashSet<>();
+//        val.add(12);
+//        val.add(23);
+//        val.add(1);
+//        val.add(23);
+//        val.add(12);
+//
+//        HashSet<Integer> res = new HashSet<>(val);
+//        System.out.println(res);
+
+        int[] arr ={3,2,2,3};
+        int val =3;
+        List<Integer> res = removeElement(arr, val);
         System.out.println(res);
+
 
     }
 
--- src/com/self/practise/Example2.java
+++ src/com/self/practise/Example2.java
@@ -5,11 +5,37 @@ import java.util.stream.Collectors;
 
 
 public class Example2 {
+
+
+    public static ArrayList<Integer> majorityElementII(ArrayList<Integer> arr) {
+        // Write your code here.
+        int n = arr.size()/3;
+        ArrayList<Integer> res = new ArrayList<>();
+        Map<Integer, Integer> val = new HashMap<Integer,Integer>();
+        for(int i :arr){
+            Integer j = val.get(i);
+            val.put(i,(j == null) ? 1 :j+1);
+        }
+        for(Map.Entry<Integer,Integer> j :val.entrySet()){
+            if(j.getValue()>n){
+                res.add(j.getKey());
+            }
+        }
+        return res;
+}
+
     public static void main(String[] args) {
-//        ArrayList<Integer> val = new ArrayList<>();
+        ArrayList<Integer> val = new ArrayList<>();
 //        val.add(12);
 //        val.add(10);
-//        val.add(32);
+//        val.add(32);7 4 4 9 7
+        val.add(7);
+        val.add(4);
+        val.add(4);
+        val.add(9);
+        val.add(7);
+        //val.add(2);
+        //val.add(3);
 //
 //
 //        List<Integer> res = val.stream().sorted().toList();
@@ -21,13 +47,16 @@ public class Example2 {
 //        min.ifPresent(vale -> System.out.println(vale));
 //        max.ifPresent(vale -> System.out.println(vale));
 
-        List<Integer> list = Arrays.asList( 3,11, 12, 2, 5);
-        Integer res2 = list.stream().min(Integer::compare).get();
+//        List<Integer> list = Arrays.asList( 3,11, 12, 2, 5);
+//        Integer res2 = list.stream().min(Integer::compare).get();
+//
+//            System.out.println(res2);
+//
+//        Integer res3 = list.stream().max(Integer::compare).get();
+//
+//        System.out.println(res3);
+        System.out.println( majorityElementII(val));
 
-            System.out.println(res2);
 
-        Integer res3 = list.stream().max(Integer::compare).get();
-
-        System.out.println(res3);
     }
 }
